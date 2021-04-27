94db53f69e985ab8cc10d36692f1d112042c081e
unknown <ivan.agustin.garcia17@gmail.com>
2021-04-27 17:56:24 +0100

    added required scripts

---
+++ Assets/States.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class WaitState : State
+{
+    public override void Enter ()
+    {
+        owner.GetComponent<Seek>().enabled = false;
+    }
+    public override void Think(){
+
+    }
+
+    public override void Exit() {
+
+    }
+}
+class FetchState : State
+{
+    public override void Enter ()
+    {
+        owner.GetComponent<Seek>().targetGameObject = GameObject.Find("Main Camera").GetComponent<Shooter>().targetBall;
+        owner.GetComponent<Seek>().enabled = true;
+    }
+    public override void Think(){
+        if(GameObject.Find("Main Camera").GetComponent<Shooter>().returned != false){
+             owner.ChangeState(new ReturnState());
+        }
+
+    }
+
+    public override void Exit() {
+        owner.GetComponent<Seek>().enabled = false;
+    }
+}
+
+class ReturnState : State
+{
+    public override void Enter ()
+    {
+        owner.GetComponent<Seek>().targetGameObject = owner.GetComponent<DogController>().person;
+        owner.GetComponent<Seek>().enabled = true;
+    }
+    public override void Think(){
+        if (Vector3.Distance(owner.GetComponent<Seek>().targetGameObject.transform.position,
+        owner.transform.position) <= 10){
+            
+        }
+    }
+
+    public override void Exit() {
+        owner.GetComponent<Seek>().enabled = false;
+    }
+}
